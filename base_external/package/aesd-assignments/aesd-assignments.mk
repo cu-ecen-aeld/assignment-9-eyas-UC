78acf2b3bd4988dd4bd5156d9bb743813a1a26db
Eyas Elhashmi <eyas077@gmail.com>
2026-08-04 22:17:45 +0100

    update the righ server

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,8 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'a4250f656e90ffb3067c55cfdec0ae4ae5d99d9b' # assignment-8 in assignments-3 repo
+# needed to get the modified sever that writes into /dev/aesdchar
+AESD_ASSIGNMENTS_VERSION = '9ec9ddfdce24be69d3580f472bac068ee6413c8f' # assignment-8 in assignments-3 repo
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -20,10 +21,8 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/usr/bin/aesdchar.ko
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
 	$(INSTALL) -m 0755 $(@D)/server/S99aesdsocket $(TARGET_DIR)/etc/init.d/S99aesdsocket
-
 endef
 
 $(eval $(generic-package))
