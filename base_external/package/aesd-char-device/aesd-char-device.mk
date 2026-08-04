402ef309665a5a5b8c03ee582d97b4bb47bbb68f
Eyas Elhashmi <eyas077@gmail.com>
2026-08-04 18:46:33 +0100

    add aesd-char-device.mk

---
+++ base_external/package/aesd-char-device/aesd-char-device.mk
@@ -0,0 +1,35 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD-CHAR-DEVICE_VERSION = '8e7d3fdad3b3ffbdb098ce8f70c1cae096554fad' # assignment-8 in assignments-3 repo
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD-CHAR-DEVICE_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-eyas-UC.git'
+AESD-CHAR-DEVICE_SITE_METHOD = git
+AESD-CHAR-DEVICE_GIT_SUBMODULES = YES
+
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+define AESD-CHAR-DEVICE_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server clean
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AESD-CHAR-DEVICE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/usr/bin/aesdchar.ko
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -m 0755 $(@D)/aesd-char-device/S97aesdchardevice $(TARGET_DIR)/etc/init.d/S97aesdchardevice
+
+endef
+
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
+
