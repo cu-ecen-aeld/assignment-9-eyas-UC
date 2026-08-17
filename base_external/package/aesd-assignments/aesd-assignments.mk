7273873f7d3dc9efdc3cb9f0001acc232b9932f4
Eyas Elhashmi <eyas077@gmail.com>
2026-08-17 16:03:48 +0100

    aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 # needed to get the modified sever that writes into /dev/aesdchar
-AESD_ASSIGNMENTS_VERSION = '196de1e84b8747da1151eb46584994446d493bbe' # assignment-9 in assignments-3 repo
+AESD_ASSIGNMENTS_VERSION = 'c57c27227c494d5044c43afb35cfd43924348286' # assignment-9 in assignments-3 repo
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
