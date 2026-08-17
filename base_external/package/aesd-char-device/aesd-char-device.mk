8ff8c578a1621b542cd002c07f41195bf148134a
Eyas Elhashmi <eyas077@gmail.com>
2026-08-18 00:31:49 +0100

    update hash of aesd-char-dev

--- base_external/package/aesd-char-device/aesd-char-device.mk
+++ base_external/package/aesd-char-device/aesd-char-device.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_DEVICE_VERSION = '4c572bb63da474cd1126c3960a7631e601a8ecdb' # assignment-8 in assignments-3 repo
+AESD_CHAR_DEVICE_VERSION = '0ce4d9d85e2cc6265b1d50678392b746e216a8b6' # assignment-8 in assignments-3 repo
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
